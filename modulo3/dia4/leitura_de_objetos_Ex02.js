78cf46a22543dc25179e53d11577be07b8b0b445
Caian Maestrelli <djcaian@gmail.com>
2023-04-04 10:28:41 -0300

    Exercicios leitura da objetos

---
+++ modulo3/dia4/leitura_de_objetos_Ex02.js
@@ -0,0 +1,23 @@
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  let gosto2 = {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  }
+
+  for (novoFavorito in leitor.livrosFavoritos){
+    Object.assign(leitor.livrosFavoritos, gosto2)
+  }
+  console.log(leitor.livrosFavoritos);
